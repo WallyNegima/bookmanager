3ac73e06c62e7c018ed7d05ae394092fc3671e22
y5RjpEqY <gfxv5e@gmail.com>
2017-06-23 14:06:09 +0900

    本の貸出

--- bookmanager_app/app/models/ledger.rb
+++ bookmanager_app/app/models/ledger.rb
@@ -2,5 +2,5 @@ class Ledger < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  scope :on_loan, where(returned_at: nil)
+  # scope :on_loan, where(returned_at: nil)
 end
--- bookmanager_app/db/migrate/20170622080102_create_books.rb
+++ bookmanager_app/db/migrate/20170622080102_create_books.rb
@@ -1,7 +1,7 @@
 class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
-      t.integer :isbn
+      t.integer :isbn , null: false
       t.string :author
       t.string :title
       t.text :description
