4b8c169572e5776593273fda15ec48d7886675c3
y5RjpEqY <gfxv5e@gmail.com>
2017-06-28 00:53:37 +0900

    とりあえずISBN手入力でAPI叩いて本の情報を登録 ISBNをstring型に変更 エラー時のflashが出せない

--- bookmanager_app/app/controllers/books_controller.rb
+++ bookmanager_app/app/controllers/books_controller.rb
@@ -4,9 +4,40 @@ class BooksController < ApplicationController
   end
 
   def create
+    isbn = params[:isbn]
+
+    # URIを指定する
+    uri = URI.parse('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn)
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    res = https.start {
+      https.get(uri.request_uri)
+    }
+
+    if res.code.to_s == '200'
+      result = JSON.parse(res.body, {:symbolize_names => true})
+      if result[:totalItems] == 0
+        flash.now[:alert] = 'Some errors occured'
+        render :new
+        return
+      end
+      result = result[:items][0][:volumeInfo]
+      book = Book.new(isbn: isbn,
+               author: result[:authors].join(','),
+               title: result[:title],
+               description: result[:description])
+      if book.save
+        redirect_to book_path(book.id)
+      end
+    else
+      flash.now[:alert] = 'Server errors'
+      render :new
+    end
   end
 
   def new
+    flash.now[:alert] = 'Server errors'
+
   end
 
   def edit
